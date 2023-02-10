1ba628e7f4f313aec9bb33f8fdcfb0dfb731ae2d
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-10 23:49:50 +0100

    test method that throw an Exception

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -10,11 +10,21 @@ namespace TestNinja.UnitTests
     public class ErrorLoggerTests
     {
         [Fact]
-        public void ErrorLogger_WhenCalled_ReturnTheLastErrorProperty()
+        public void ErrorLogger_ErrorNotNull_ReturnTheLastErrorProperty()
         {
             var logger = new ErrorLogger();
             logger.Log("a");
             Assert.Equal("a", logger.LastError);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void ErrorLogger_ErrorIsNull_ThrowAnException(string error)
+        {
+            var logger = new ErrorLogger();
+            Assert.Throws<ArgumentNullException>(()=>logger.Log(error));
         }
     }
 }
