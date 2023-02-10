266a750dda9c503b20c5629a3790f002b48669b0
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-10 18:28:18 +0100

    create test for Reservation class

--- TestNinja/TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -4,6 +4,38 @@ namespace TestNinja.UnitTests
 {
     public class ReservationTests
     {
-       
+        [Fact]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true});
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanBeCancelledBy_ReservationOwner_ReturnTrue()
+        {
+            //Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy= user};
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CanBeCancelledBy_SomeOneElse_ReturnFalse()
+        {
+            //Arrange
+            var reservation = new Reservation { MadeBy = new User()};
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+            //Assert
+            Assert.False(result);
+        }
     }
 }
