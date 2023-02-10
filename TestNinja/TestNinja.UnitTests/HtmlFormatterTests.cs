0f33b01ccbf4fc01c6186e0ae9b25ca000940e8d
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-10 21:41:24 +0100

    create unit test for string

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class HtmlFormatterTests
+    {
+        [Fact]
+        public void FormatAsBold_WhenCalled_ShouldReturnBoldString()
+        {
+            //Arrange
+            var formatter = new HtmlFormatter();
+            //Act
+            var result = formatter.FormatAsBold("abc");
+            //Assert
+            // MORE SPECIFIC
+            //Assert.Equal("<strong>abc</strong>", result);
+            // MORE GENERAL
+            Assert.StartsWith("<strong>", result);
+            Assert.EndsWith("</strong>", result);
+            Assert.Contains("abc", result);
+        }
+    }
+}
