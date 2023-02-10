1c5b8d52c6f8baf957b22717d5b850a04ccd9fcb
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-10 23:00:30 +0100

    test the return type of method

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public void GetCustomer_WhenIdIsZero_ReturnNotFound()
+        {
+            var customerController = new CustomerController();
+
+            var result = customerController.GetCustomer(0);
+
+            Assert.IsType<NotFound>(result);
+        }
+
+        [Fact]
+        public void GetCustomer_WhenIsNotZero_ReturnOk()
+        {
+            var customerController = new CustomerController();
+
+            var result = customerController.GetCustomer(1);
+
+            Assert.IsType<Ok>(result);
+        }
+    }
+}
