71acc1dc769fed1272d712af60becd70edc4ff17
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-10 23:32:54 +0100

    test a method that return void

---
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class ErrorLoggerTests
+    {
+        [Fact]
+        public void ErrorLogger_WhenCalled_ReturnTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.Equal("a", logger.LastError);
+        }
+    }
+}
