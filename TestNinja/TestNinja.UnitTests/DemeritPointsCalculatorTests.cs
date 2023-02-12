a739cdf4ec29631418e98cd465ccaeaa1f359847
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-12 14:27:55 +0100

    create a unit test for DemeritPointsCalculator exercice

---
+++ TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    public class DemeritPointsCalculatorTests
+    {
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(301)]
+        public void CalculateDemeritPoints_WithWrongParametre_ThrowException(int speed)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            Assert.Throws<ArgumentOutOfRangeException>(()=> demeritPointsCalculator.CalculateDemeritPoints(speed));
+        }
+
+        [Fact]
+        public void CalculateDemeritPoints_SpeedLessToSpeedLimit_Return0()
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            var result = demeritPointsCalculator.CalculateDemeritPoints(65);
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void CalculateDemeritPoints_SpeedIs70_Return1()
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            var result = demeritPointsCalculator.CalculateDemeritPoints(70);
+            Assert.Equal(1, result);
+        }
+    }
+}
