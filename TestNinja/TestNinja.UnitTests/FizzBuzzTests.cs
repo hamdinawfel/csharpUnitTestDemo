59c7e468b1aa2d308e617e39de68cb00204a40c5
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-12 13:48:53 +0100

    create a unit test for  a fizzbuzz exercice

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(15,"FizzBuzz")]
+        [InlineData(3,"Fizz")]
+        [InlineData(5,"Buzz")]
+        [InlineData(1,"1")]
+        public void GetOutput_WhenCalles_ReturnTheCorrectString(int number, string expected)
+        {
+            var result = FizzBuzz.GetOutput(number);
+            Assert.Equal(expected, result);
+        }
+    }
+}
