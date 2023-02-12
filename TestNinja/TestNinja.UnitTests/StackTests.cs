3b15ec1c53f4b94362606e9df151a4102dd8f821
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2023-02-12 15:45:26 +0100

    refactor the unit testing of Stack excercice

--- TestNinja/TestNinja.UnitTests/StackTests.cs
+++ TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -9,7 +9,7 @@ namespace TestNinja.UnitTests
     public class StackTests
     {
         [Fact]
-        public void Push_NotNullObj_AddObjToList()
+        public void Push_NotNullObj_AddObjToStack()
         {
             var stack = new Fundamentals.Stack<string>();
             stack.Push("abc");
@@ -18,14 +18,29 @@ namespace TestNinja.UnitTests
         }
 
         [Fact]
-        public void Push_NullObj_ThrowsException()
+        public void Push_AddNullObjToStack_ThrowArgumentNullException()
         {
             var stack = new Fundamentals.Stack<string>();
             Assert.Throws<ArgumentNullException>(() => stack.Push(null));
         }
 
         [Fact]
-        public void Pop_WithNotEmptyList_RemoveTheLastItemOfThatList()
+        public void Count_WithEmptyStack_ReturnZero()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void Count_WithNotEmptyStack_Return1()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("abc");
+            Assert.Equal(1, stack.Count);
+        }
+
+        [Fact]
+        public void Pop_WithNotEmptyStack_ReturnTheLastItemOfThatStack()
         {
             var stack = new Fundamentals.Stack<string>();
             stack.Push("a");
@@ -36,14 +51,23 @@ namespace TestNinja.UnitTests
         }
 
         [Fact]
-        public void Pop_WithEmptyList_ThrowsInvalidOperationException()
+        public void Pop_WithNotEmptyStack_RemoveTheLastItemOfThatStack()
+        {
+            var stack = new Fundamentals.Stack<string>();
+            stack.Push("a");
+            stack.Pop();
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void Pop_WithEmptyStack_ThrowsInvalidOperationException()
         {
             var stack = new Fundamentals.Stack<string>();
             Assert.Throws<InvalidOperationException>(()=> stack.Pop());
         }
 
         [Fact]
-        public void Peek_WithNotEmptyList_ReturnTheLastItemOfThatList()
+        public void Peek_WithNotEmptyStack_ReturnTheLastItemOfThatStack()
         {
             var stack = new Fundamentals.Stack<string>();
             stack.Push("a");
@@ -52,7 +76,7 @@ namespace TestNinja.UnitTests
         }
 
         [Fact]
-        public void Peek_WithEmptyList_ThrowsInvalidOperationException()
+        public void Peek_WithEmptyStack_ThrowsInvalidOperationException()
         {
             var stack = new Fundamentals.Stack<string>();
             Assert.Throws<InvalidOperationException>(() => stack.Peek());
